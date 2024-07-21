9fee81beb6dd49cc5d2198577be80bf2655ac2fe
NKdevboy <nithishkumarramup@gmail.com>
2024-07-21 15:55:34 +0530

    Update ConfigurableInterface.h

--- ConfigurableInterface.h
+++ ConfigurableInterface.h
@@ -1,3 +1,5 @@
 #define MAJORCOLORNAMES {"White", "Red", "Black", "Yellow", "Violet"}
+#define MAJORCOLORNAMESSIZE 5
 #define MINORCOLORNAMES {"Blue", "Orange", "Green", "Brown", "Slate"}
+#define MINORCOLORNAMESSIZE 5
 #define MAX_COLORPAIR_NAME_CHARS 16
