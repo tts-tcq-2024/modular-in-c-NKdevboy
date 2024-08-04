175b30ee4fdfc5393f8b6ed12cbfd22c0474b2d8
Nithishkumar R (MS/ESG1-VM) <R.Nithishkumar@in.bosch.com>
2024-08-04 15:34:16 +0530

    Data type issue

--- manual_generator.c
+++ manual_generator.c
@@ -1,6 +1,7 @@
 #include "ConfigurableInterface.h"
 #include "manual_generator.h"
 #include "Application.h"
+#include "ImplementationDataTypes.h"
 #include <stdio.h>
 
 extern const char* MajorColorNames[];
@@ -11,7 +12,7 @@ void ManualGenerator(void)
     for (int i = 1; i <= MAJORCOLORNAMESSIZE * MINORCOLORNAMESSIZE; ++i) 
     {
         char colorPairNames[MAX_COLORPAIR_NAME_CHARS]={0};
-        int colorPair = GetColorFromPairNumber(i);
+        ColorPair colorPair = GetColorFromPairNumber(i);
         ColorPairToString(&colorPair, colorPairNames);
         printf("%d) %s\n",colorPair,colorPairNames);
     }
